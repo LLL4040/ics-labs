6b44174d27271acb6001611d50bc6462f4386a4c
LLL4040 <2293114926@qq.com>
2019-03-27 23:58:08 +0800

    add files for online2

---
+++ online2/target79/try.d
@@ -0,0 +1,10 @@
+
+try.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 43 41 37 53 	mov    $0x53374143,%rdi
+   7:	68 a7 17 40 00       	pushq  $0x4017a7
+   c:	c3                   	retq   
---
+++ online2/target79/try.s
@@ -0,0 +1,3 @@
+mov $0x53374143,%rdi
+push $0x4017a7
+ret
